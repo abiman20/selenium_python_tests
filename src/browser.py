d0bb4cbd4ec81b2fe3e5ad7608a88b1c3701b779
LAPTOP-4A5K00VD\user <nagamanickam.m@gmail.com>
2023-03-31 16:57:19 +0100

    added function

--- src/browser.py
+++ src/browser.py
@@ -5,44 +5,24 @@ import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("https://www.saucedemo.com")
+def login(page,username,password):
+    page.get("https://www.saucedemo.com")
+    username_txt = page.find_element(By.ID,'user-name')
+    password_txt = page.find_element(By.NAME,'password')
+    login_btn =page.find_element(By.CLASS_NAME,'submit-button.btn_action')
+    username_txt.send_keys(username)
+    password_txt.send_keys(password)
+    login_btn.click()
 
-username_txt = driver.find_element(By.ID,'user-name')
-
-password_txt = driver.find_element(By.NAME,'password')
-
-login_btn =driver.find_element(By.CLASS_NAME,'submit-button.btn_action')
-
-username_txt.send_keys('standard_user')
-
-password_txt.send_keys('secret_sauce')
-
-login_btn.click()
-
+# ------------login with correct user-------------
+login(driver,'standard_user','secret_sauce')
 assert driver.current_url == 'https://www.saucedemo.com/inventory.html',driver.current_url
-
 assert driver.find_element(By.CLASS_NAME,'title').text == 'Products','title is not matching'
-
 time.sleep(2)
 
-driver.get("https://www.saucedemo.com")
-
-username_txt = driver.find_element(By.ID,'user-name')
-
-password_txt = driver.find_element(By.NAME,'password')
-
-login_btn =driver.find_element(By.CLASS_NAME,'submit-button.btn_action')
-
-username_txt.send_keys('locked_out_user')
-
-password_txt.send_keys('secret_sauce')
-
-login_btn.click()
-
-
-
+# ------------login with incorrect user----------
+login(driver,'locked_out_user','secret_sauce')
 assert driver.find_element(By.CLASS_NAME,'error-message-container.error').text == 'Epic sadface: Sorry, this user has been locked out.','error text is not matching'
-
 time.sleep(2)
 
 driver.quit()
