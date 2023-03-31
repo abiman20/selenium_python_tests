a00e6d3d8dabbd09c6300e6d377ead61e2b69d50
LAPTOP-4A5K00VD\user <nagamanickam.m@gmail.com>
2023-03-31 13:17:54 +0100

    automated sauce lab login page

--- src/browser.py
+++ src/browser.py
@@ -5,20 +5,44 @@ import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("https://tutorialshut.com/demo-website-for-selenium-automation-practice/")
+driver.get("https://www.saucedemo.com")
 
-element = driver.find_element(By.ID,"fname")
+username_txt = driver.find_element(By.ID,'user-name')
 
-element.send_keys("abi")
+password_txt = driver.find_element(By.NAME,'password')
 
-button = driver.find_element(By.ID,"idOfButton")
-button.click()
+login_btn =driver.find_element(By.CLASS_NAME,'submit-button.btn_action')
 
-result = driver.find_element(By.ID,'result1')
+username_txt.send_keys('standard_user')
 
-result_text = result.text
+password_txt.send_keys('secret_sauce')
 
-assert result_text == 'You clicked on submit Button ::Yes', result_text
+login_btn.click()
+
+assert driver.current_url == 'https://www.saucedemo.com/inventory.html',driver.current_url
+
+assert driver.find_element(By.CLASS_NAME,'title').text == 'Products','title is not matching'
 
 time.sleep(2)
+
+driver.get("https://www.saucedemo.com")
+
+username_txt = driver.find_element(By.ID,'user-name')
+
+password_txt = driver.find_element(By.NAME,'password')
+
+login_btn =driver.find_element(By.CLASS_NAME,'submit-button.btn_action')
+
+username_txt.send_keys('locked_out_user')
+
+password_txt.send_keys('secret_sauce')
+
+login_btn.click()
+
+
+
+assert driver.find_element(By.CLASS_NAME,'error-message-container.error').text == 'Epic sadface: Sorry, this user has been locked out.','error text is not matching'
+
+time.sleep(2)
+
 driver.quit()
