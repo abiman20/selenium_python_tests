70b9874b6c7a9aad4e6cedbf4957c88269b455ad
abiman20 <abi.manickam20@gmail.com>
2023-04-28 11:45:10 +0100

    Added functions

---
+++ practice/functions.py
@@ -0,0 +1,100 @@
+# Function is a block of code which only runs when it is called
+# You can pass data, known as parameters into a function
+# A function can return data as a result
+
+# 1.cerating a function - using def as a keyword
+def welcome():
+    print('welcome to python')
+# calling a function - function name followed by parenthesis
+welcome()
+welcome()
+welcome()
+
+# 2.No return type without argument function
+def add():
+    a = int(input('Enter the value of a:'))
+    b = int(input('Enter the value of b:'))
+    c = a+b
+    print('Total:',c)
+add()
+
+# 3.No return type with argument function 
+def sub(a,b):
+    c = a-b
+    print('Difference:',c)
+sub(25,3)
+
+# 4.Return type without argument function
+def mul():
+    a = int(input('Enter the value of a:'))
+    b = int(input('Enter the value of b:'))
+    c = a*b
+    return c
+x = mul()
+print('Mul:',x)
+
+# 5.Return type with argument function
+def div(a,b):
+    c = a/b
+    return(c)
+x = div(6,2)
+print('Div:',x)
+print('Div:',div(12,2))
+
+# 6.Arbitrary arguments function(*)
+# we can pass n number of arguments
+def class_1(*students):
+    print(students)
+class_1('ram','sam','raj','krish')
+print(type(class_1))
+
+# 7.using for loop 
+def class_1(*students):
+    print(students)
+    for user in students:
+        print(user)
+class_1('ram','sam','raj','krish')
+
+# 8.Keyword argument function
+#using variable name as keyword
+def student(name,age):
+    print(name,' age is ',age)
+student(age=25,name='ram')
+
+# 8.Arbitrary keyword arguments in python(**)
+def class_10(**data):
+    print(data)
+    for i in data:
+        print(i)
+class_10(name='ram',age=25,gender='male')
+
+# 9.Default parameter value
+# if we call the function without argument, it uses the default value
+def user(name,city='salem'):
+    print(name,' is from ',city)
+user('ram','erode')
+user('sam')
+
+# 10.Passing list as argument
+def total(marks):
+    return sum(marks)
+print('Total:',total([55,67,89,76,98]))
+
+# 11.Recursive function - a function can call itself
+# a defined function can call itself
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return(x*factorial(x-1))
+print('factorial:',factorial(5))
+
+'''
+factorial(5)
+5*factorial(4)
+5*4*factorial(3)
+5*4*3*factorial(2)
+5*4*3*2*factorial(1)
+5*4*3*2*1
+120
+'''
